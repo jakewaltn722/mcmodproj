8b14c4bd449abb639c71ad68f0e9dc0a3e82d335
jakewaltn722 <jakewaltn722@gmail.com>
2026-05-21 23:57:41 -0700

    initial commit attempt 2

---
+++ MultiLoader-Template-26.1.2/common/src/main/java/pig12/eggmod/Constants.java
@@ -0,0 +1,11 @@
+package pig12.eggmod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "eggmod";
+	public static final String MOD_NAME = "Egg Mod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
